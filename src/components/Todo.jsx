1b74831c106515efd579066b4f23af7bd1587ee7
Andrew Mbugua <andrewmbugua76@gmail.com>
2022-05-17 02:02:38 -0700

    css changes

--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -2,12 +2,14 @@ import React,{useState} from 'react'
 import TodoForm from './TodoForm'
 
 
-function Todo({todos, completeTodo ,removeTodo, updateTodo}) {
+function Todo({todos, handleChange, completeTodo ,removeTodo, updateTodo   }) {
 
     const[edit,setEdit]= useState({
     id:null,
     value :'',
 });
+
+
 
 const submitUpdate = value =>{
     updateTodo(edit.id , value)
@@ -33,10 +35,21 @@ return todos.map((todo  , index ) =>(
 </div>
 
   <div className=''>
-      <input  type="checkbox" value="checked" />
+
+        <input 
+        className="form-check-input  m-2" 
+        style={{margin:"0 10px"}}
+         type="checkbox" 
+         value="" 
+         id="flexCheckDefault"
+          checked={todos.done}
+          onChange={()=>handleChange(todo.id)}
+           />
+           <span style={{textDecoration:"lineThrough"}}  >{todo.title}</span>
+
   <button type='button' onClick={()=> setEdit({id:todo.id , value:todo.text})} className="btn btn-warning" > Edit </button>
 
-      <button onClick={()=>removeTodo(todo.id)} className="btb btn-danger"type='button' >X</button>
+      <button onClick={()=>removeTodo(todo.id)} className="btb btn-danger m-2 "type='button' >delete</button>
   </div> 
 
 </div>
